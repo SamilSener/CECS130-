d7ea6b7da3ee2ccd8434829bc52f7f7340d598c1
SamilSener <36458820+SamilSener@users.noreply.github.com>
2018-02-13 16:38:44 -0500

    Add files via upload

---
+++ CECSLab1Sener_Samil.c
@@ -0,0 +1,37 @@
+//  Lab1
+//  This program is a representation of Dr. Yampolsky
+//  Sener Samil CECS130 Section 1
+
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("           **********\n");
+    printf("           **0****0**\n");
+    printf("           ****()****\n");
+    printf("           **VVVVVV**\n");
+    printf("           **********\n");
+    printf("               **\n");
+    printf("               **\n");
+    printf("               **\n");
+    printf("              ****\n");
+    printf("            ** ** **\n");
+    printf("           **  **  **\n");
+    printf("         **    **   **\n");
+    printf("        **     **    **\n");
+    printf("               **\n");
+    printf("               **\n");
+    printf("               **\n");
+    printf("              ****\n");
+    printf("            ** ** **\n");
+    printf("           **  **  **\n");
+    printf("         **    **   **\n");
+    printf("        **     **    **\n");
+    printf("       **      **     **\n");
+    printf("     **        **      **\n");
+    printf("   #####       **     #####\n");
+
+    return 0;
+}
+
